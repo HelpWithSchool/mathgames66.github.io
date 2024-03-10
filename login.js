03e5a23828ea999c2cfc1e81b19b02a1fa39e8ad
HelpWithSchool <133970634+HelpWithSchool@users.noreply.github.com>
2024-03-10 17:00:32 -0400

    DZHHDGJFFS

---
+++ login.js
@@ -0,0 +1,29 @@
+// Define user credentials
+var users = [
+    { username: 'user1', password: 'password1' },
+    { username: 'user2', password: 'password2' },
+    { username: 'user3', password: 'password3' }
+  ];
+  
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // Get the values from the form fields
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+    // Check user credentials
+    var authenticatedUser = users.find(function(user) {
+      return user.username === username && user.password === password;
+    });
+  
+    if (authenticatedUser) {
+      alert('Login successful');
+  
+      // Open a new window/tab with the desired HTML page
+      var newWindow = window.open('about:blank');
+      newWindow.location.href = 'games.html';
+    } else {
+      alert('Invalid username or password');
+    }
+  });
